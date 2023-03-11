9858e5bb869d7b9f834b03490203851ba8f51f11
Jjuhui313 <hyacinth92928@gmail.com>
2023-03-11 11:19:53 +0900

    chore: 초기 세팅

---
+++ src/test/java/com/sparta/daydeibackrepo/DaydeiBackRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.daydeibackrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaydeiBackRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

c0c372e6d7987c628056eb5d29817ea0e4d97a28
the1224 <asdz2003@gmail.com>
2023-03-11 15:38:59 +0900

    feat : JWT 기능 email로 수정

---
+++ src/main/java/com/sparta/daydeibackrepo/security/UserDetailsImpl.java
@@ -0,0 +1,67 @@
+package com.sparta.daydeibackrepo.security;
+
+import com.sparta.daydeibackrepo.user.entity.User;
+import com.sparta.daydeibackrepo.user.entity.UserRoleEnum;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class UserDetailsImpl implements UserDetails {
+
+    private final User user;
+    private final String username;
+
+    public UserDetailsImpl(User user, String username) {
+        this.user = user;
+        this.username = username;
+    }
+
+    public User getUser() {   //Getter가 없어서 안씀
+        return user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        UserRoleEnum role = user.getRole();
+        String authority = role.getAuthority();
+
+        SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority(authority);
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(simpleGrantedAuthority);
+
+        return authorities;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}

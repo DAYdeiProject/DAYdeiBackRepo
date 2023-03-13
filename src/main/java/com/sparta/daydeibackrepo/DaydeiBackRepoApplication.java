3751da468f57b3593cb3a46e22131a0ab60a41bd
Jjuhui313 <hyacinth92928@gmail.com>
2023-03-13 14:24:57 +0900

    fix: application 파일 수정

---
+++ src/main/java/com/sparta/daydeibackrepo/DaydeiBackRepoApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.daydeibackrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaydeiBackRepoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaydeiBackRepoApplication.class, args);
+    }
+
+}

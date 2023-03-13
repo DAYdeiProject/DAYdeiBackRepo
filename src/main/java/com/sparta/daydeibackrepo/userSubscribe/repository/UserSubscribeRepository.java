f376bb2eb97f97788b954cd37cc473b9e57ec60c
the1224 <asdz2003@gmail.com>
2023-03-13 09:53:28 +0900

    feat : UserSubscribe 관련 Create, Delete 구현

--- src/main/java/com/sparta/daydeibackrepo/userSubscribe/repository/UserSubscribeRepository.java
+++ src/main/java/com/sparta/daydeibackrepo/userSubscribe/repository/UserSubscribeRepository.java
@@ -1,4 +1,11 @@
 package com.sparta.daydeibackrepo.userSubscribe.repository;
 
-public interface UserSubscribeRepository {
+import com.sparta.daydeibackrepo.user.entity.User;
+import com.sparta.daydeibackrepo.userSubscribe.entity.UserSubscribe;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserSubscribeRepository extends JpaRepository<UserSubscribe, Long> {
+    Optional<UserSubscribe> findBySubscribingIdAndSubscriberId(User subscribing, User subscriber);
 }

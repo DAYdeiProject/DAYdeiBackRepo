011f7c4ee31f5733cc4cc9b5c8810850b258dd90
the1224 <asdz2003@gmail.com>
2023-03-15 22:15:02 +0900

    feat : 공유일정 생성, 수락, 거절 기능 구현

--- src/main/java/com/sparta/daydeibackrepo/postSubscribe/controller/PostSubscribeController.java
+++ src/main/java/com/sparta/daydeibackrepo/postSubscribe/controller/PostSubscribeController.java
@@ -1,4 +1,29 @@
 package com.sparta.daydeibackrepo.postSubscribe.controller;
 
+import com.sparta.daydeibackrepo.postSubscribe.service.PostSubscribeService;
+import com.sparta.daydeibackrepo.security.UserDetailsImpl;
+import com.sparta.daydeibackrepo.util.StatusResponseDto;
+import io.swagger.v3.oas.annotations.Parameter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/posts/subscribes")
 public class PostSubscribeController {
+
+    private final PostSubscribeService postSubscribeService;
+
+    @PutMapping("/{postId}")
+    public StatusResponseDto<String> approveJoin(@PathVariable Long postId, @Parameter(hidden = true) @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        postSubscribeService.approveJoin(postId, userDetails);
+        return StatusResponseDto.success("일정을 수락하였습니다.");
+    }
+
+    @DeleteMapping("/{postId}")
+    public StatusResponseDto<String> rejectJoin(@PathVariable Long postId, @Parameter(hidden = true) @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        postSubscribeService.rejectJoin(postId, userDetails);
+        return StatusResponseDto.success("일정을 거절하였습니다.");
+    }
 }

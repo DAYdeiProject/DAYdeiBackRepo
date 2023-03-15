994547db6e4f0913aefd992ee8a4baa684bb550e
Jjuhui313 <hyacinth92928@gmail.com>
2023-03-15 17:15:07 +0900

    chore: swagger 입력값에 UserDetails 나타나지 않도록 @Parameter(hidden = true) 추가

--- src/main/java/com/sparta/daydeibackrepo/post/controller/PostController.java
+++ src/main/java/com/sparta/daydeibackrepo/post/controller/PostController.java
@@ -5,6 +5,7 @@ import com.sparta.daydeibackrepo.post.dto.PostResponseDto;
 import com.sparta.daydeibackrepo.post.service.PostService;
 import com.sparta.daydeibackrepo.security.UserDetailsImpl;
 import com.sparta.daydeibackrepo.util.StatusResponseDto;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -17,7 +18,7 @@ public class PostController {
     private final PostService postService;
 
     @PostMapping("posts")
-    public StatusResponseDto<PostResponseDto> createPost(@RequestBody PostRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+    public StatusResponseDto<PostResponseDto> createPost(@RequestBody PostRequestDto requestDto, @Parameter(hidden = true) @AuthenticationPrincipal UserDetailsImpl userDetails) {
         return StatusResponseDto.success(postService.createPost(requestDto, userDetails));
     }
 

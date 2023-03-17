78509ae3cc465c79d29573dd3290e99a8e363e28
Jiyoon <51226340+trueyoon@users.noreply.github.com>
2023-03-17 18:25:48 +0900

    fix: 정리

--- src/main/java/com/sparta/daydeibackrepo/user/dto/UserInfoRequestDto.java
+++ src/main/java/com/sparta/daydeibackrepo/user/dto/UserInfoRequestDto.java
@@ -10,7 +10,7 @@ public class UserInfoRequestDto {
     private String nickName;
     private String newPassword;
     private String newPasswordConfirm;
-    private String profileImage;
+    private String profileImage; //todo 타입 수정
     private String introduction;
     private String birthday;
 }

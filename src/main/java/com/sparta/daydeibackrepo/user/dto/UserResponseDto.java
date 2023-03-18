67f397acfc2a98a9ebd0d46c2b5f6973c791c4d8
the1224 <asdz2003@gmail.com>
2023-03-18 20:13:40 +0900

    feat : 친구 추천 알고리즘 구현

--- src/main/java/com/sparta/daydeibackrepo/user/dto/UserResponseDto.java
+++ src/main/java/com/sparta/daydeibackrepo/user/dto/UserResponseDto.java
@@ -2,6 +2,7 @@ package com.sparta.daydeibackrepo.user.dto;
 
 import com.sparta.daydeibackrepo.user.entity.CategoryEnum;
 import com.sparta.daydeibackrepo.user.entity.User;
+import com.sparta.daydeibackrepo.user.repository.UserRepository;
 import com.sparta.daydeibackrepo.userSubscribe.entity.UserSubscribe;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,7 +22,10 @@ public class UserResponseDto {
     Boolean friendCheck;
     Boolean isRequestFriend;
     Boolean userSubscribeCheck;
-    public UserResponseDto(User user, boolean friendCheck,boolean isRequestFriend, boolean userSubscribeCheck){
+    int friendCount;
+    int subscribingCount;
+    int subscriberCount;
+    public UserResponseDto(User user, boolean friendCheck,boolean isRequestFriend, boolean userSubscribeCheck, int friendCount, int subscribingCount, int subscriberCount){
         this.id = user.getId();
         this.email = user.getEmail();
         this.nickName = user.getNickName();
@@ -30,8 +34,11 @@ public class UserResponseDto {
         this.friendCheck = friendCheck;
         this.isRequestFriend = isRequestFriend;
         this.userSubscribeCheck = userSubscribeCheck;
+        this.friendCount = friendCount;
+        this.subscribingCount = subscribingCount;
+        this.subscriberCount = subscriberCount;
     }
-    public UserResponseDto(User user, boolean friendCheck, boolean userSubscribeCheck){
+    public UserResponseDto(User user, boolean friendCheck, boolean userSubscribeCheck, int friendCount, int subscribingCount, int subscriberCount){
         this.id = user.getId();
         this.email = user.getEmail();
         this.nickName = user.getNickName();
@@ -39,6 +46,9 @@ public class UserResponseDto {
         this.categoryList = user.getCategoryEnum();
         this.friendCheck = friendCheck;
         this.userSubscribeCheck = userSubscribeCheck;
+        this.friendCount = friendCount;
+        this.subscribingCount = subscribingCount;
+        this.subscriberCount = subscriberCount;
     }
     public UserResponseDto(User user, boolean friendCheck){
         this.id = user.getId();
@@ -47,6 +57,7 @@ public class UserResponseDto {
         this.profileImage = user.getProfileImage();
         this.categoryList = user.getCategoryEnum();
         this.friendCheck = friendCheck;
+
     }
 
     public UserResponseDto(UserSubscribe userSubscribe, boolean userSubscribeCheck){

8cbd35e337425845c03c79e18f951af910ca6b2e
the1224 <asdz2003@gmail.com>
2023-03-18 18:07:22 +0900

    fix : FriendRepository 관련 에러 수정

--- src/main/java/com/sparta/daydeibackrepo/friend/repository/FriendRepository.java
+++ src/main/java/com/sparta/daydeibackrepo/friend/repository/FriendRepository.java
@@ -9,20 +9,17 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 
-public interface FriendRepository extends JpaRepository<Friend, Long> {
-    Friend findByFriendRequestIdAndFriendResponseId(User requestUser, User responseUser);
+public interface FriendRepository extends JpaRepository<Friend, Long>, FriendCustomRepository {
+    /*Friend findByFriendRequestIdAndFriendResponseId(User requestUser, User responseUser);*/
     Optional<Friend> findById(Long id);
 
-    @Query("SELECT f FROM Friend f WHERE (f.friendRequestId = :user "+" OR f.friendResponseId =:user) "+" AND f.friendCheck = true")
+    /*@Query("SELECT f FROM Friend f WHERE (f.friendRequestId = :user "+" OR f.friendResponseId =:user) "+" AND f.friendCheck = true")
     List<Friend> findFriends(User user);
 
     @Query("SELECT f FROM Friend f WHERE (f.friendRequestId = :user1 "+" AND f.friendResponseId =:user2 "+" AND f.friendCheck = true) "+" OR (f.friendRequestId = :user2 "+" AND f.friendResponseId =:user1 "+" AND f.friendCheck = true)")
     Friend findFriend(User user1, User user2);
 
     @Query("SELECT f FROM Friend f WHERE f.friendRequestId = :user1 "+" AND f.friendResponseId =:user2 "+" AND f.friendCheck = false")
-    Friend findFirstOneRequest(User user1, User user2);
-
-    @Query("SELECT f FROM Friend f WHERE (f.friendResponseId.id = :id) "+" AND f.friendRequestId = :user "+" AND f.friendResponseId !=:user")
-    List<Friend> findidFriendList(Long id, User user);
+    Friend findFirstOneRequest(User user1, User user2);*/
 
 }

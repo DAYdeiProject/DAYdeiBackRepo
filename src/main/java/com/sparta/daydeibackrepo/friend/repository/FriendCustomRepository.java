da11f39e09f203a044d7270efe17454f057cc75a
the1224 <asdz2003@gmail.com>
2023-03-18 17:24:44 +0900

    feat : friend 관련 Query문 JPQL > QueryDSL로 변경

--- src/main/java/com/sparta/daydeibackrepo/friend/repository/FriendCustomRepository.java
+++ src/main/java/com/sparta/daydeibackrepo/friend/repository/FriendCustomRepository.java
@@ -7,5 +7,8 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 import java.util.Optional;
 public interface FriendCustomRepository {
-
+    Friend findByFriendRequestIdAndFriendResponseId(User requestUser, User responseUser);
+    List<Friend> findFriends(User user);
+    Friend findFriend(User user1, User user2);
+    Friend findFirstOneRequest(User user1, User user2);
 }
--- src/main/java/com/sparta/daydeibackrepo/friend/repository/FriendCustomRepositoryImpl.java
+++ src/main/java/com/sparta/daydeibackrepo/friend/repository/FriendCustomRepositoryImpl.java
@@ -1,14 +1,48 @@
 package com.sparta.daydeibackrepo.friend.repository;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.sparta.daydeibackrepo.friend.entity.Friend;
 import com.sparta.daydeibackrepo.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+import static com.sparta.daydeibackrepo.friend.entity.QFriend.friend;
+
 @Repository
 @RequiredArgsConstructor
 public class FriendCustomRepositoryImpl implements FriendCustomRepository  {
     private final JPAQueryFactory jpaQueryFactory;
 
+    public Friend findByFriendRequestIdAndFriendResponseId(User requestUser, User responseUser){
+        return jpaQueryFactory
+                .selectFrom(friend)
+                .where(friend.friendRequestId.eq(requestUser).and(friend.friendResponseId.eq(responseUser)))
+                .fetchFirst();
+    }
+
+    //@Query("SELECT f FROM Friend f WHERE (f.friendRequestId = :user "+" OR f.friendResponseId =:user) "+" AND f.friendCheck = true")
+    public List<Friend> findFriends(User user){
+        return jpaQueryFactory
+                .selectFrom(friend)
+                .where(friend.friendRequestId.eq(user).or(friend.friendResponseId.eq(user)).and(friend.friendCheck.eq(true)))
+                .fetch();
+    }
+    //@Query("SELECT f FROM Friend f WHERE (f.friendRequestId = :user1 "+" AND f.friendResponseId =:user2 "+" AND f.friendCheck = true) "+" OR (f.friendRequestId = :user2 "+" AND f.friendResponseId =:user1 "+" AND f.friendCheck = true)")
+    public Friend findFriend(User user1, User user2){
+        return jpaQueryFactory
+                .selectFrom(friend)
+                .where((friend.friendRequestId.eq(user1).and(friend.friendResponseId.eq(user2)).and(friend.friendCheck.eq(true)))
+                        .or(friend.friendRequestId.eq(user2).and(friend.friendResponseId.eq(user1)).and(friend.friendCheck.eq(true))))
+                .fetchFirst();
+    }
+    //@Query("SELECT f FROM Friend f WHERE f.friendRequestId = :user1 "+" AND f.friendResponseId =:user2 "+" AND f.friendCheck = false")
+    public Friend findFirstOneRequest(User user1, User user2){
+        return jpaQueryFactory
+                .selectFrom(friend)
+                .where(friend.friendRequestId.eq(user1).and(friend.friendResponseId.eq(user2)).and(friend.friendCheck.eq(false)))
+                .fetchFirst();
+    }
 }

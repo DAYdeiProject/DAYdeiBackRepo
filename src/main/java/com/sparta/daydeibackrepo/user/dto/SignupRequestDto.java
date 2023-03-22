9a0d6bd2276b3dd54ce14df8d8e976b55ad3de1e
the1224 <asdz2003@gmail.com>
2023-03-22 09:22:09 +0900

    fix : SignupRequestDto 회원가입 시 요구조건 추가

--- src/main/java/com/sparta/daydeibackrepo/user/dto/SignupRequestDto.java
+++ src/main/java/com/sparta/daydeibackrepo/user/dto/SignupRequestDto.java
@@ -3,17 +3,20 @@ package com.sparta.daydeibackrepo.user.dto;
 
 import lombok.Getter;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @Getter
 public class SignupRequestDto {
-
+    @Email
     private String email;
     @Pattern(regexp = "(?=.*?[a-zA-Z])(?=.*?[\\d])(?=.*?[~!@#$%^&*()_+=\\-`]).{8,15}")
     private String password;
     private String passwordCheck;
-
+    @Pattern(regexp = ".{1,6}")
     private String nickName;
-
+    @NotNull
     private String birthday;
 }

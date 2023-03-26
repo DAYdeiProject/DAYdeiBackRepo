3360de307c8a4869ea493620709326791dc44948
Jjuhui313 <hyacinth92928@gmail.com>
2023-03-27 01:37:56 +0900

    chore: ssl 적용 후 swagger에 https 도메인 문제없이 접근되도록 @OpenAPIDefinition 추가

--- src/main/java/com/sparta/daydeibackrepo/DaydeiBackRepoApplication.java
+++ src/main/java/com/sparta/daydeibackrepo/DaydeiBackRepoApplication.java
@@ -1,5 +1,7 @@
 package com.sparta.daydeibackrepo;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -8,6 +10,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableJpaAuditing
 @EnableScheduling
+@OpenAPIDefinition(servers = {@Server(url = "https://daydei.shop", description = "Default Server URL") ,@Server(url = "http://localhost:8080", description = "Local Server URL")})
 @EnableAsync
 @SpringBootApplication
 public class DaydeiBackRepoApplication {

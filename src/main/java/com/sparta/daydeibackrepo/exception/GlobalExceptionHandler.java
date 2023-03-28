6a67167bae202941a8626f115293efec5150058d
Jjuhui313 <hyacinth92928@gmail.com>
2023-03-28 18:00:23 +0900

    chore: Sentry 의존성 추가 및 Sentry로 예외를 보내는 코드 추가

--- src/main/java/com/sparta/daydeibackrepo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sparta/daydeibackrepo/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.sparta.daydeibackrepo.exception;
 
 
 import com.sparta.daydeibackrepo.util.StatusResponseDto;
+import io.sentry.Sentry;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,28 +17,17 @@ import static com.sparta.daydeibackrepo.exception.message.ExceptionMessage.NOT_L
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-//    @ExceptionHandler({IllegalArgumentException.class, NullPointerException.class})
-//    @ResponseStatus(HttpStatus.BAD_REQUEST)
-//    public StatusResponseDto<?> handle(Exception ex) {
-//        ex.printStackTrace();
-//        return StatusResponseDto.fail(HttpStatus.BAD_REQUEST, ex.getMessage());
-//    }
-
-//    @ExceptionHandler(Exception.class)
-//    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-//    public StatusResponseDto<?> handleException(Exception ex) {
-//        return StatusResponseDto.fail(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
-//    }
-
     @ExceptionHandler(value = {CustomException.class})
     protected ResponseEntity<StatusResponseDto> handleCustomException(CustomException e) {
         log.error("handleCustomException throw CustomException : {}", e.getExceptionMessage());
+        Sentry.captureException(e); // Sentry로 예외를 보냅니다.
         return StatusResponseDto.toExceptionResponseEntity(e.getExceptionMessage());
     }
 
     //정규식
     @ExceptionHandler({BindException.class})
     public ResponseEntity<StatusResponseDto> bindException(BindException ex) {
+        Sentry.captureException(ex); // Sentry로 예외를 보냅니다.
         return StatusResponseDto.toAllExceptionResponseEntity(HttpStatus.BAD_REQUEST,
                 ex.getFieldError().getDefaultMessage());
     }
@@ -45,19 +35,16 @@ public class GlobalExceptionHandler {
     //토큰 없을시
     @ExceptionHandler({MissingRequestHeaderException.class})
     public ResponseEntity<StatusResponseDto> missingRequestHeaderException(MissingRequestHeaderException ex) {
+        Sentry.captureException(ex); // Sentry로 예외를 보냅니다.
         return StatusResponseDto.toAllExceptionResponseEntity(NOT_LOGGED_ID);
     }
 
     // 500
     @ExceptionHandler({Exception.class})
     public ResponseEntity<StatusResponseDto> handleAll(final Exception ex) {
+        Sentry.captureException(ex); // Sentry로 예외를 보냅니다.
         return StatusResponseDto.toAllExceptionResponseEntity(HttpStatus.BAD_REQUEST, ex.getMessage());
     }
-//    @ExceptionHandler({BoughtException.class})
-//    protected ResponseEntity<StatusResponseDto> handleCustomRollBackException(BoughtException e) {
-//        log.error("handleCustomException throw CustomException : {}", e.getExceptionMessage());
-//        return StatusResponseDto.toAllExceptionResponseEntity(e.getExceptionMessage(),e.getObject());
-//    }
 
 
 }

d198775cb4d1c1d6ccc02c02a1ada3714473d9e5
Jjuhui313 <hyacinth92928@gmail.com>
2023-03-31 02:45:13 +0900

    chore: 서버 테스트

--- src/main/java/com/sparta/daydeibackrepo/friend/controller/FriendController.java
+++ src/main/java/com/sparta/daydeibackrepo/friend/controller/FriendController.java
@@ -22,7 +22,6 @@ public class FriendController {
     public StatusResponseDto<FriendResponseDto> requestFriend(@PathVariable Long userId, @Parameter(hidden = true) @AuthenticationPrincipal UserDetailsImpl userDetails){
         return StatusResponseDto.success(friendService.requestFriend(userId, userDetails));
     }
-
     @PutMapping("/{userId}")
     public StatusResponseDto<FriendResponseDto> setFriend(@PathVariable Long userId, @Parameter(hidden = true) @AuthenticationPrincipal UserDetailsImpl userDetails){
         return StatusResponseDto.success(friendService.setFriend(userId, userDetails));

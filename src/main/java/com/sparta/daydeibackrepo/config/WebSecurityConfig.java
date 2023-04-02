cb1ee291548b1e46302eeec616f3ef4f6a5fb3df
Jjuhui313 <hyacinth92928@gmail.com>
2023-04-02 18:19:03 +0900

    chore: https 적용한 도메인 주소 cors 추가

--- src/main/java/com/sparta/daydeibackrepo/config/WebSecurityConfig.java
+++ src/main/java/com/sparta/daydeibackrepo/config/WebSecurityConfig.java
@@ -87,6 +87,7 @@ public class WebSecurityConfig implements WebMvcConfigurer {
         config.addAllowedOrigin("https://daydei.vercel.app/");
         config.addAllowedOrigin("https://jeongda.shop/");
         config.addAllowedOrigin("https://sparta-daln.shop/");
+        config.addAllowedOrigin("https://daydei.life/");
 //        //채팅 테스트용 origin
         config.addAllowedOrigin("http://localhost:8080");
 

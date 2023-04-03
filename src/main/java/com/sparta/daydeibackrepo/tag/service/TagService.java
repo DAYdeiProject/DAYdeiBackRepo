510ed4ba3278f44fea6e774137d9e18919519a1f
the1224 <asdz2003@gmail.com>
2023-04-03 13:37:32 +0900

    fix : 카카오 유저 친구맺기 카운트 추가 / 컬러 이넘 변경 / 친구 태그시 일정 충돌여부 체크

--- src/main/java/com/sparta/daydeibackrepo/tag/service/TagService.java
+++ src/main/java/com/sparta/daydeibackrepo/tag/service/TagService.java
@@ -3,7 +3,11 @@ package com.sparta.daydeibackrepo.tag.service;
 import com.sparta.daydeibackrepo.exception.CustomException;
 import com.sparta.daydeibackrepo.friend.repository.FriendRepository;
 import com.sparta.daydeibackrepo.friend.service.FriendService;
+import com.sparta.daydeibackrepo.post.entity.Post;
+import com.sparta.daydeibackrepo.post.repository.PostRepository;
 import com.sparta.daydeibackrepo.security.UserDetailsImpl;
+import com.sparta.daydeibackrepo.tag.dto.TagRequestDto;
+import com.sparta.daydeibackrepo.tag.dto.TagResponseDto;
 import com.sparta.daydeibackrepo.user.dto.UserResponseDto;
 import com.sparta.daydeibackrepo.user.entity.User;
 import com.sparta.daydeibackrepo.user.repository.UserRepository;
@@ -11,8 +15,14 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.sparta.daydeibackrepo.exception.message.ExceptionMessage.UNAUTHORIZED_MEMBER;
 
@@ -23,13 +33,29 @@ public class TagService {
     private final UserRepository userRepository;
     private final FriendRepository friendRepository;
     private final FriendService friendService;
+    private final PostRepository postRepository;
 
-    public List<UserResponseDto> getFriendTagList(String searchWord, UserDetailsImpl userDetails) {
+    // 시작, 종료 일자 시간도 받아와야함.
+    public List<TagResponseDto> getFriendTagList(TagRequestDto tagRequestDto, UserDetailsImpl userDetails) {
         User user = userRepository.findByEmail(userDetails.getUsername()).orElseThrow(
                 () -> new CustomException(UNAUTHORIZED_MEMBER)
         );
-        List<User> tagList = friendRepository.findTagUser(user, searchWord);
-        List<UserResponseDto> tagResponseDtos = friendService.makeUserResponseDtos(user,tagList);
+        List<TagResponseDto> tagResponseDtos = new ArrayList<>();
+        LocalDate startDate = LocalDate.parse(tagRequestDto.getStartDate(), DateTimeFormatter.ISO_DATE);
+        LocalDate endDate = LocalDate.parse(tagRequestDto.getEndDate(), DateTimeFormatter.ISO_DATE);
+        LocalTime startTime = LocalTime.parse(tagRequestDto.getStartTime());
+        LocalTime endTime = LocalTime.parse(tagRequestDto.getEndTime());
+        List<User> postUsers = postRepository.findAll().stream()
+                .filter(Post -> LocalDateTime.of(startDate,startTime).isBefore(LocalDateTime.now()) && LocalDateTime.of(endDate,endTime).isAfter(LocalDateTime.now()))
+                .map(post -> post.getUser())
+                .collect(Collectors.toList());
+        List<User> tagList = friendRepository.findTagUser(user, tagRequestDto.getSearchWord());
+        for (User user1 : tagList){
+            boolean scheduleCheck = false;
+            if(postUsers.contains(user1))
+            {scheduleCheck = true;}
+            tagResponseDtos.add(new TagResponseDto(user1, scheduleCheck));
+        }
         Collections.shuffle(tagResponseDtos);
         return tagResponseDtos;
     }

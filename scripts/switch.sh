696d4f1445015348052759dc1cfbf0ee0e7fda47
Jiyoon <51226340+trueyoon@users.noreply.github.com>
2023-04-04 17:26:05 +0900

    Changes by ktc system

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -1,16 +1,31 @@
 #!/usr/bin/env bash
+# switch.sh
 
-ABSPATH=$(readlink -f $0)
-ABSDIR=$(dirname $ABSPATH)
-source ${ABSDIR}/profile.sh
+# service_url.inc 에서 현재 서비스를 하고 있는 WAS의 포트 번호 가져오기
+CURRENT_PORT=$(cat /home/ubuntu/service_url.inc  | grep -Po '[0-9]+' | tail -1)
+TARGET_PORT=0
 
-function switch_proxy() {
-    IDLE_PORT=$(find_idle_port)
+echo "> Nginx currently proxies to ${CURRENT_PORT}."
 
-    echo "> 전환할 Port: $IDLE_PORT"
-    echo "> Port 전환"
-    echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
+if [ ${CURRENT_PORT} -eq 8081 ]; then
+    TARGET_PORT=8082
+elif [ ${CURRENT_PORT} -eq 8082 ]; then
+    TARGET_PORT=8081
+else
+    echo "> No WAS is connected to nginx"
+    exit 1
+fi
 
-    echo "> 엔진엑스 Reload"
-    sudo service nginx reload
-}
+# 위 커맨드들을 통해 현재 타겟포트 가져오기
+
+# $ service_url.inc 파일을 현재 바뀐 서버의 포트로 변경
+echo "set \$service_url http://127.0.0.1:${TARGET_PORT};" | tee /home/ubuntu/service_url.inc
+
+echo "> Now Nginx proxies to ${TARGET_PORT}."
+
+# nginx를 reload 해준다.
+sudo service nginx reload
+
+echo "> Nginx reloaded."
+
+fuser -k ${CURRENT_PORT}/tcp

4ef7e3da0ff91f9e74b9252902ead2af87decebf
Jiyoon <51226340+trueyoon@users.noreply.github.com>
2023-04-14 17:42:19 +0900

    test: UserServiceTest

--- src/test/java/com/sparta/daydeibackrepo/user/service/UserServiceTest.java
+++ src/test/java/com/sparta/daydeibackrepo/user/service/UserServiceTest.java
@@ -1,6 +1,11 @@
 package com.sparta.daydeibackrepo.user.service;
 
+import com.sparta.daydeibackrepo.jwt.JwtUtil;
+import com.sparta.daydeibackrepo.mail.service.MailService;
+import com.sparta.daydeibackrepo.notification.repository.NotificationRepository;
+import com.sparta.daydeibackrepo.s3.service.S3Service;
 import com.sparta.daydeibackrepo.user.dto.SignupRequestDto;
+import com.sparta.daydeibackrepo.user.entity.User;
 import com.sparta.daydeibackrepo.user.repository.UserRepository;
 import com.sparta.daydeibackrepo.util.StatusResponseDto;
 import org.apache.http.HttpStatus;
@@ -17,35 +22,43 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 @ExtendWith(MockitoExtension.class)
-
 class UserServiceTest {
 
     @InjectMocks
     private UserService userService;
     @Mock
     private UserRepository userRepository;
-//    @Mock
-//    private PasswordEncoder passwordEncoder;
+    @Mock
+    private PasswordEncoder passwordEncoder;
+    @Mock
+    private JwtUtil jwtUtil;
+    @Mock
+    private MailService mailService;
+    @Mock
+    private S3Service s3Service;
+    @Mock
+    private NotificationRepository notificationRepository;
 
     @Test
     void signup_Success() {
         // Given
         SignupRequestDto signupRequestDto = new SignupRequestDto();
         signupRequestDto.setEmail("test@example.com");
-        signupRequestDto.setPassword("myPassword123*");
-        signupRequestDto.setPasswordCheck("myPassword123*");
+        signupRequestDto.setPassword("password123*");
+        signupRequestDto.setPasswordCheck("password123*");
         signupRequestDto.setNickName("nickname");
         signupRequestDto.setBirthday("0101");
-
+        User user = new User(signupRequestDto.getEmail(), signupRequestDto.getPassword(), signupRequestDto.getNickName(), signupRequestDto.getBirthday());
         when(userRepository.findByEmail(anyString())).thenReturn(Optional.empty());
-
+        when(userRepository.save(any(User.class))).thenReturn(user);
+//        when(passwordEncoder.encode(signupRequestDto.getPassword())).thenReturn(passwordEncoder.encode(signupRequestDto.getPassword()));
+//        when(passwordEncoder.encode(signupRequestDto.getPasswordCheck())).thenReturn(passwordEncoder.encode(signupRequestDto.getPasswordCheck()));
         // When
         StatusResponseDto<?> response = userService.signup(signupRequestDto);
 
         // Then
         assertNotNull(response);
         assertEquals(HttpStatus.SC_OK, response.getStatusCode());
-        assertEquals("SIGN_UP_SUCCESS", response.getData());
     }
 
     @Test
